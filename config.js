36886e6808525dea963a25e969643198ef89516b
oppemu <oppemu@mahidol.ac.th>
2026-09-24 11:37:06 +0700

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwLq8c5vRm0wuc1h9asfGX1xKR1f3h2ny-4wvmHZZpkwPQBVv-60M4EFpV3dZCiPc63Vw/exec"
-};
-const WEB_APP_URL = CONFIG.GOOGLE_SCRIPT_URL;
