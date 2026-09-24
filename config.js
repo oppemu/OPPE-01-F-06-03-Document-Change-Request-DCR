6f668e80d9db74295a39d0d3841363475417e7c3
oppemu <oppemu@mahidol.ac.th>
2026-09-24 09:50:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,1 @@
-const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzOYuNKmq3VbeOgy1B2tdS1wZzm24b48KolGFr1F7vUY9R891qTrU8LlqZ1lNUDTBwz0A/exec"
-};
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwLq8c5vRm0wuc1h9asfGX1xKR1f3h2ny-4wvmHZZpkwPQBVv-60M4EFpV3dZCiPc63Vw/exec";
